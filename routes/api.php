a47e3ce62bd087db404512b4db21ae3b86be5faa
Andres Chaparro <andres.chaparro7@hotmail.com>
2025-09-12 22:34:03 -0500

    Created basic routes for login and register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route to login into the system, available for both the web and mobile clients
+Route::post("login", [AuthController::class, "login"])->name("login");
+
+//Route to register new users MOBILE ONLY
+Route::post("register", [AuthController::class, "register"])->name("register");
