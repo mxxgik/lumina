5189d2b0ecdcbe50c422e1abcb507489e7566833
Andres Chaparro <andres.chaparro7@hotmail.com>
2025-09-12 23:34:44 -0500

    Included AuthController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 //Route to login into the system, available for both the web and mobile clients
 Route::post("login", [AuthController::class, "login"])->name("login");
