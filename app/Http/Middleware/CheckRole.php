67816f3e235355b8f57cf9c742531e17f6c3c29a
mxxgik <andres.chaparro7@hotmail.com>
2025-10-10 22:56:18 -0500

    Added a role checking middleware for routing

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $role): Response
+    {
+        if (!$request->user() || ($request->user()->role !== $role && $request->user()->role !== 'admin')) {
+            return response()->json([
+                'response_code' => 403,
+                'success' => false,
+                'message' => 'Unauthorized - Insufficient permissions'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
