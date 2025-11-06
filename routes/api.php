6eb48e2ca132596d7a3205d3c8e4c4e6e1d66730
mxxgik <andres.chaparro7@hotmail.com>
2025-11-05 23:27:33 -0500

    Started work on routing, still wip

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,56 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AprendizController;
+use App\Http\Controllers\AprendizElementoAdicionalController;
+use App\Http\Controllers\AprendizEquipoController;
+use App\Http\Controllers\ElementoAdicionalController;
+use App\Http\Controllers\EquipoAdicionalController;
+use App\Http\Controllers\EquipoOElementoController;
+use App\Http\Controllers\FormacionController;
+use App\Http\Controllers\HistorialController;
+use App\Http\Controllers\HistorialElementoAdicionalController;
+use App\Http\Controllers\TipoProgramaController;
 
-//Route to login into the system, available for both the web and mobile clients
-Route::post("login", [AuthController::class, "login"])->name("login");
+//Authentication related routes
+Route::prefix('auth')->group(function ()
+{
+    //Login available for both mobile and desktop web clients
+    Route::post("login", [AuthController::class, "login"])->name("login");
+    
+    //Register only available for mobile client
+    Route::post("register", [AuthController::class, "register"])->name("register");
+    
+    //Logout route, available for both mobile and web clients
+    Route::middleware(['auth:sanctum'])->post("logout", [AuthController::class, "logout"])->name("logout");
+});
 
-//Route to register new users MOBILE ONLY
-Route::post("register", [AuthController::class, "register"])->name("register");
+//Routes for 'portero' role
+Route::middleware(['auth:sanctum', 'role:portero'])->group(function ()
+{
+    Route::prefix('portero')->group(function () {
+        Route::prefix('historial')->group(function () {
+            Route::get("show", [HistorialController::class, "show"]);
+            Route::post("create", [HistorialController::class, "store"]);
+        }); 
+    });
+});
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    //TODO: agregar roles
-    Route::prefix('admin')->group(function () {});
-    Route::prefix('portero')->group(function () {});
-    Route::prefix('aprendiz')->group(function () {});
+//Routes for 'aprendiz' role
+Route::middleware(['auth:sanctum', 'role:aprendiz'])->group(function ()
+{
+    Route::prefix('aprendiz')->group(function () {
+        Route::prefix('')->group(function () {
+        }); 
+    });
+});
+
+//Routes for 'admin' role
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function ()
+{
+    Route::prefix('admin')->group(function () {
+        Route::prefix('historial')->group(function () {
+        }); 
+    });
+
 });
