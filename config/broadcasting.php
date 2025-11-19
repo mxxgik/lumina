ae54a21f2a0bc5e59d8eb5646f0e3fc1427bf6e9
mxxgik <andres.chaparro7@hotmail.com>
2025-11-18 21:58:35 -0500

    Changed options for pusher broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,14 +35,10 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusherapp.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-            ],
+                'options' => [
+                  'cluster' => 'us2',
+                  'useTLS' => true
+                ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
