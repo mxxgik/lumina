5e5f9454cbef2247f72040ed9e81b438a414f604
mxxgik <andres.chaparro7@hotmail.com>
2025-11-19 23:28:17 -0500

    Reverted config changes

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'sanctum'),
+        'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -39,11 +39,6 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
-        ],
-
-        'sanctum' => [
-            'driver' => 'sanctum',
-            'provider' => null,
         ],
     ],
 
