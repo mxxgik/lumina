57c0cf3ea002a8c28187839b9f4107e3356ed1cb
mxxgik <andres.chaparro7@hotmail.com>
2025-11-21 21:46:16 -0500

    Added DB::listen() to the boot method to start a listenener which logs the id of the authed user, then sends a query to log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,11 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
+
+        // Log all database queries
+        DB::listen(function ($query) {
+            $user = auth()->check() ? auth()->user()->id . ' (' . auth()->user()->email . ')' : 'guest';
+            Log::channel('db')->info("User: {$user} - Query: {$query->sql} - Bindings: " . json_encode($query->bindings) . " - Time: {$query->time}ms");
+        });
     }
 }
