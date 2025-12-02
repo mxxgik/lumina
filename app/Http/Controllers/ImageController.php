100c87d644cf7e383c923e17948cb0687793f703
mxxgik <andres.chaparro7@hotmail.com>
2025-12-02 18:42:12 -0500

    Testing error code responses

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -33,6 +33,6 @@ class ImageController extends Controller
             return response()->json(['error' => 'Image not found inside of public path'], 404);
         }
 
-        return Storage::disk('public')->response($path);
+        #return Storage::disk('public')->response($path, 200);
     }
 }
